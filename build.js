eafcf36273250998cdf500d9d0b7acffbce65a99
Tom <36381958+simple-development@users.noreply.github.com>
2019-03-23 22:41:19 +0100

    test

--- build.js
+++ build.js
@@ -1,7 +1,51 @@
 
+console.log('!!!');
+
 const fs = require('fs');
 
-fs.writeFileSync("index.html", "This is a test!!");
+
+
+async function readDir (path) {
+    try {
+        var content = await fs.readdirSync(path, {withFileTypes: true});
+
+        for (var i = 0; i < content.length; i++) {
+            // if folder
+            if (content[i].isDirectory() && content[i].name.charAt(0) !== '.') {
+                //console.log(content[i].name);
+                readDir(path + content[i].name + '/');
+
+            } else if (content[i].name === 'index.txt') {
+                var str = fs.readFileSync(path + 'index.txt');
+                fs.writeFileSync(path + 'index.html', str);
+                console.log('created: ' + path + 'index.html');
+            }
+        }
+
+
+        //console.log(content);
+    } catch (error) {
+        console.log(error);
+    }
+
+    //fs.writeFileSync("index.html", "This is a test!!!");
+}
+
+
+
+
+async function build () {
+    try {
+        readDir("./");
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+
+build();
+
 
 
 
